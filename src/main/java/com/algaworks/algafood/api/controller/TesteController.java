fd32fef5b63e6acca3d3e02f25d8d5fda36d61f3
Rodrigo da Silva Marques <CIANDT\rodrigomarques@lnb029465bhz.ciandt.global>
2022-03-28 07:31:32 -0300

    Estendendo JpaRepository para customização de um repositorio base.

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -72,4 +72,10 @@ public class TesteController {
 		return restauranteRepository.findComFreteGratis(nome);
 	}
 	
+	@GetMapping("/restaurantes/primeiro") 
+	public Optional<Restaurante> restaurantePrimeiro() {
+		return restauranteRepository.buscarPrimeiro();
+		
+	}
+	
 }
